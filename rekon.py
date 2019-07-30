1a0eac67822f4344fa1eee13379b7157ae2ac908
Thais Marques <thais.hfmarques@gmail.com>
2019-07-30 19:02:48 -0300

    innitial commit

---
+++ rekon.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# author: @thaishfmarques
+# 
+# imported modules
+from os import system
+from sys import exit
+
+system('clear')
+print('			R E K O N  v0.1')
+print('	| Welcome to Rekon v0.1 an open source tool			\n' 
+			'	| for reconnaissance and information gathering	\n')
+
+while True:
+	print('		[1]\n'
+				'		[2]\n'
+				'		[3]\n'
+				'		[4]\n'
+				'		[5] Exit\n')
+	input()
