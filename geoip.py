d6185c57d32d9021f3cd2868b7f56f03d34d2b21
Thais Marques <thais.hfmarques@gmail.com>
2019-08-03 10:18:24 -0300

    added key

--- geoip.py
+++ geoip.py
@@ -6,11 +6,11 @@ get_input = '200.147.3.157'
 lat = '-26.196223'
 lngt= '-52.689523'
 response = requests.get('http://api.hackertarget.com/geoip/?q=' + get_input)
-map = requests.get('http://maps.googleapis.com/maps/api/geocode/xml?key=AIzaSyBvz9DkTVjFdrYuYZu8ytPp9q-uk5i3gYg?latlng='+str(lat)+','+lngt)
+get_map = requests.get('https://maps.googleapis.com/maps/api/geocode/xml?key=AIzaSyBvz9DkTVjFdrYuYZu8ytPp9q-uk5i3gYg&latlng='+str(lat)+','+lngt)
 
 if response.status_code == 200:
-	print(response.text)
-	print(map.text)
+	#print(response.text)
+	print(get_map.text)
 else:
 	print('Invalid')
 
