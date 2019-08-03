6fe77eaed6db789ded26a9457a7e0f4db1dae803
Thais Marques <thais.hfmarques@gmail.com>
2019-08-03 10:15:35 -0300

    excluido disclaimer

--- whois.py
+++ whois.py
@@ -4,9 +4,11 @@ import requests
 
 get_input = 'fiap.com.br'
 response = requests.get('http://api.hackertarget.com/whois/?q=' + get_input)
-
-
+test = response.text
 if response.status_code == 200:
-	print(response.text)
+	#print(response.text)
+	for line in test.split('\n'):
+		if not line.startswith('%'):
+			print(line)
 else:
 	print('Invalid')
