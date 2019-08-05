bb867d3fb975d6238cb650ced161ecec6817e1ff
Thais Marques <thais.hfmarques@gmail.com>
2019-08-04 22:34:23 -0300

    alterado nome do arquivo principal

---
+++ bin/informant.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# author: @thaishfmarques
+#
+# TODO: Avoid getter and setter methods. (clean get_)
+# TODO: Importing order: System imports Third-party imports Local source tree imports
+
+# imported modules
+#from os import system # importa funcao de comandos unix
+#from time import sleep
+import banner
+
+menu_option=''
+banner.menu_splash(menu_option)
