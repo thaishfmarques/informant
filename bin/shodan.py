09ddf628099adc567e2922db79b9408d43dbe452
Thais Marques <thais.hfmarques@gmail.com>
2019-08-08 19:58:45 -0300

    teste

---
+++ bin/shodan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# author: @thaishfmarques
+
+from shodan import Shodan
+from shodan.cli.helpers import get_api_key
+
+api = Shodan(get_api_key())
+
+limit = 100
+counter = 0
+
+
+for banner in api.search_cursor('product:mongodb'):
+    print(banner['data'])
+
+    counter +=1
+    if counter >= limit:
+        break
